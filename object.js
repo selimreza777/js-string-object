7a029bee0bf016c8e249f72dc820d729abac3357
selimreza777 <selimrezacse@gmail.com>
2025-08-03 18:49:13 +0600

    object commit one

---
+++ object.js
@@ -0,0 +1,31 @@
+
+//primitive
+//basic, primary data type
+const age = 21;
+const school = 'rifel square';
+const isPassed = true;
+let isDeveloper;
+// console.log(isDeveloper);
+
+const subjects = ['bangla', 'english', 'math', 'physic', 'ict'];
+
+// object declear korar tips 
+//non-primitive 
+const bottle = {
+  brand: 'Akij',
+  price: 70,
+  color: 'white',
+  isFresh: false,
+}
+
+//non-primitive
+const subjects ={
+  courseName: 'Bangla',
+  teacher:'Khairul Bashar',
+  examDate: '30 Feb',
+  chapters:['first', 'second', 'third'],
+  exam:{
+    name: 'Final Exam',
+    marks: 100
+  } 
+}
