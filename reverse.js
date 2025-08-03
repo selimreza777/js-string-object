31f519a6fcc828ca1e57e7a23437c2dd2cf4749f
selimreza777 <selimrezacse@gmail.com>
2025-08-03 17:11:28 +0600

    reverse ok

--- reverse.js
+++ reverse.js
@@ -8,3 +8,17 @@ for (const letter of sentences) {
   reverse = letter + reverse;
 }
 // console.log(reverse);
+
+//ignore this solution
+let rev = '';
+for (let i = 0; i < sentences.length; i++) {
+  // console.log(sentences[i]);
+  const letter = sentences[i];
+  rev = letter + rev;
+}
+// console.log(rev);
+
+
+//another shortcut solution reverses
+const reversed = sentences.split('').reverse().join('');
+console.log(reversed);
