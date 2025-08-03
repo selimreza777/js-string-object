a00f704af6a56ab8a38ff8bde95735ea35dc646f
selimreza777 <selimrezacse@gmail.com>
2025-08-03 21:05:58 +0600

    values done

---
+++ values.js
@@ -0,0 +1,10 @@
+const computer = {
+  brand: 'lenovo',
+  price: 35000,
+  processor: 'intel',
+  hdd: '512gb',
+  monitor: 'Dell'
+
+}
+const values = Object.values(computer); //obj er property er values debe
+console.log(values);
