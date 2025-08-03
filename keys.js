a402081f893fa88c2ccea223951f289c679b92dd
selimreza777 <selimrezacse@gmail.com>
2025-08-03 20:53:23 +0600

    keys

---
+++ keys.js
@@ -0,0 +1,10 @@
+const computer = {
+  brand: 'lenovo',
+  price: 35000,
+  processor: 'intel',
+  hdd: '512gb'
+
+}
+//joto gula property ace ta array er modde debe
+const keys = Object.keys(computer);
+console.log(keys); 
